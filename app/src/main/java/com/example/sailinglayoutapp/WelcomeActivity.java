bf38cc29ecd8463c104dbd157f2f6b6538b2d610
Andre Lautee <102115854@student.swin.edu.au>
2020-09-20 20:57:00 +1000

    Onboarding

--- app/src/main/java/com/example/sailinglayoutapp/WelcomeActivity.java
+++ app/src/main/java/com/example/sailinglayoutapp/WelcomeActivity.java
@@ -8,8 +8,10 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
@@ -74,7 +76,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent();
-                intent.setClass(getApplicationContext(), CourseVariablesActivity.class);
+                intent.setClass(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
